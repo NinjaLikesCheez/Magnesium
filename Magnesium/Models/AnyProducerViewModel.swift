60d4f215cc0e7da5b0bb1aeb5c3966515b411f34
James Hurst <james@jameshurst.ca>
2020-01-29 00:05:55 -0500

    Move AnyProducerViewModel to its own file

---
+++ Magnesium/Models/AnyProducerViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  AnyProducerViewModel.swift
+//  Magnesium
+//
+//  Created by James Hurst on 2020-01-29.
+//  Copyright © 2020 James Hurst. All rights reserved.
+//
+
+import Combine
+
+/// A type erased wrapper for a type that is both a `ViewModel` and `EventProducer`.
+public class AnyProducerViewModel<Event, ViewEvent, ViewState>: ViewModel, EventProducer {
+    private let _events: () -> AnyPublisher<Event, Never>
+    private let _state: () -> ViewState
+    private let _handle: (ViewEvent) -> Void
+    public let base: Any
+    public var state: ViewState { _state() }
+    public var events: AnyPublisher<Event, Never> { _events() }
+
+    public init<Base: ViewModel & EventProducer>(
+        _ base: Base
+    ) where Base.Event == Event, Base.ViewEvent == ViewEvent, Base.ViewState == ViewState {
+        self.base = base
+        _events = { base.events }
+        _state = { base.state }
+        _handle = base.handle
+    }
+
+    public func handle(_ event: ViewEvent) {
+        _handle(event)
+    }
+}
--- Magnesium/Models/AnyViewModel.swift
+++ Magnesium/Models/AnyViewModel.swift
@@ -28,26 +28,3 @@ public class AnyViewModel<ViewEvent, ViewState>: ViewModel, Identifiable {
         _handle(event)
     }
 }
-
-/// A type erased wrapper for a type that is both a `ViewModel` and `EventProducer`.
-public class AnyProducerViewModel<Event, ViewEvent, ViewState>: ViewModel, EventProducer {
-    private let _events: () -> AnyPublisher<Event, Never>
-    private let _state: () -> ViewState
-    private let _handle: (ViewEvent) -> Void
-    public let base: Any
-    public var state: ViewState { _state() }
-    public var events: AnyPublisher<Event, Never> { _events() }
-
-    public init<Base: ViewModel & EventProducer>(
-        _ base: Base
-    ) where Base.Event == Event, Base.ViewEvent == ViewEvent, Base.ViewState == ViewState {
-        self.base = base
-        _events = { base.events }
-        _state = { base.state }
-        _handle = base.handle
-    }
-
-    public func handle(_ event: ViewEvent) {
-        _handle(event)
-    }
-}
