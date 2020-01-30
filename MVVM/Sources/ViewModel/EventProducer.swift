fefc8a922e95f07ba5dd2080d6b5889741292ccd
James Hurst <james@jameshurst.ca>
2020-01-30 09:29:04 -0500

    EventProducer doc

--- MVVM/Sources/ViewModel/EventProducer.swift
+++ MVVM/Sources/ViewModel/EventProducer.swift
@@ -1,6 +1,6 @@
 import Combine
 
-/// An `EventProducer` emits events over time.
+/// An `EventProducer` produces events over time.
 public protocol EventProducer {
     associatedtype Event
     /// The event publisher.
