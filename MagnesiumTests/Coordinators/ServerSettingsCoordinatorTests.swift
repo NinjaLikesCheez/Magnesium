5709331820ceb30c2a167e1ef6d2cb17f2f8f3af
James Hurst <james@jameshurst.ca>
2020-02-05 21:39:11 -0500

    Added ServerSettingsCoordinator tests

---
+++ MagnesiumTests/Coordinators/ServerSettingsCoordinatorTests.swift
@@ -0,0 +1,85 @@
+//
+//  ServerSettingsCoordinatorTests.swift
+//  MagnesiumTests
+//
+//  Created by James Hurst on 2020-02-05.
+//  Copyright © 2020 James Hurst. All rights reserved.
+//
+
+import Combine
+@testable import Magnesium
+import XCTest
+
+class ServerSettingsCoordinatorTests: XCTestCase {
+    private let window = UIWindow()
+    private let preferences = MockPreferences()
+    private var coordinator: ServerSettingsCoordinator!
+    private var observers = [AnyCancellable]()
+
+    override func setUp() {
+        super.setUp()
+        coordinator = ServerSettingsCoordinator(type: .transmission, preferences: preferences)
+        // the view controller needs to be in a key window to perform a presentation
+        window.rootViewController = coordinator.presentable.viewController
+        window.makeKeyAndVisible()
+    }
+
+    // MARK: presentable
+
+    func test_presentable_withDelugeServer_shouldBeServerSettingsViewController() {
+        let coordinator = ServerSettingsCoordinator(server: .delugeMock(), preferences: preferences)
+        let viewController = coordinator.presentable.viewController
+        guard type(of: viewController) === ServerSettingsViewController<AnyServerSettingsViewModel>.self else {
+            XCTFail("Unexpected view controller: \(String(describing: viewController))")
+            return
+        }
+    }
+
+    func test_presentable_withTransmissionServer_shouldBeServerSettingsViewController() {
+        let coordinator = ServerSettingsCoordinator(server: .transmissionMock(), preferences: preferences)
+        let viewController = coordinator.presentable.viewController
+        guard type(of: viewController) === ServerSettingsViewController<AnyServerSettingsViewModel>.self else {
+            XCTFail("Unexpected view controller: \(String(describing: viewController))")
+            return
+        }
+    }
+
+    func test_presentable_withDelugeServerType_shouldBeServerSettingsViewController() {
+        let coordinator = ServerSettingsCoordinator(type: .deluge, preferences: preferences)
+        let viewController = coordinator.presentable.viewController
+        guard type(of: viewController) === ServerSettingsViewController<AnyServerSettingsViewModel>.self else {
+            XCTFail("Unexpected view controller: \(String(describing: viewController))")
+            return
+        }
+    }
+
+    func test_presentable_withTransmissionServerType_shouldBeServerSettingsViewController() {
+        let coordinator = ServerSettingsCoordinator(type: .transmission, preferences: preferences)
+        let viewController = coordinator.presentable.viewController
+        guard type(of: viewController) === ServerSettingsViewController<AnyServerSettingsViewModel>.self else {
+            XCTFail("Unexpected view controller: \(String(describing: viewController))")
+            return
+        }
+    }
+
+    // MARK: handle - ServerSettingsEvent
+
+    func test_serverSettings_completeEvent_shouldEmitCompleteEvent() {
+        var event: ServerSettingsCoordinatorEvent?
+        coordinator.events.sink { event = $0 }.store(in: &observers)
+        coordinator.handle(.complete)
+        guard case .complete = event else {
+            XCTFail("Unexpected event: \(String(describing: event))")
+            return
+        }
+    }
+
+    func test_serverSettings_alertEvent_shouldPresentAlertController() {
+        coordinator.handle(.alert(Alert(title: "", message: nil, style: .alert), source: nil))
+        let viewController = coordinator.presentable.viewController.presentedViewController!
+        guard type(of: viewController) == UIAlertController.self else {
+            XCTFail("Unexpected view controller: \(String(describing: viewController))")
+            return
+        }
+    }
+}
