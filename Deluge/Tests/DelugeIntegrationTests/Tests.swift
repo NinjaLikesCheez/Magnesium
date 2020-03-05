403e4ce5b52eed1471ea27ed4d81c9b1a3718ce5
James Hurst <james@jameshurst.ca>
2020-03-05 09:22:50 -0500

    Fixed formatting

--- Deluge/Tests/DelugeIntegrationTests/Tests.swift
+++ Deluge/Tests/DelugeIntegrationTests/Tests.swift
@@ -5,10 +5,12 @@ import XCTest
 class DelugeIntegrationTests: XCTestCase {
     enum Resources {
         static let debianHash = "5a8062c076fa85e8056451c0d9aa04349ae27909"
+        // swiftformat:disable all
         static let magnetURL = """
             magnet:?xt=urn:btih:54da0b79719064aa10fe2cc4e13630a1222d1939&dn=archlinux-2020.03.01-x86_64.iso\
             &tr=udp://tracker.archlinux.org:6969&tr=http://tracker.archlinux.org:6969/announce
             """
+        // swiftformat:enable all
         static let magnetHash = "54da0b79719064aa10fe2cc4e13630a1222d1939"
     }
 
