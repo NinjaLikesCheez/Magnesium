546f35c1987f485a07bc32ef159856d488aef52d
James Hurst <james@jameshurst.ca>
2020-03-16 20:03:24 -0400

    Add Components as a submodule

--- Libraries/Components/README.md
+++
@@ -1,7 +0,0 @@
-# Components
-
-A collection of components to build an iOS application.
-
-## License
-
-This library is released under the MIT license. See [LICENSE](LICENSE) for details.
--- Libraries/Components/Sources/ViewModel/EventEmitter.swift
+++
@@ -1,9 +0,0 @@
-import Combine
-
-/// An `EventEmitter` can emit events over time.
-public protocol EventEmitter {
-    /// The type of event being emitted.
-    associatedtype Event
-    /// A publisher that emits values of type `Event`.
-    var events: AnyPublisher<Event, Never> { get }
-}
