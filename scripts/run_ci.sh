e3662f1c334159380af63dbb370092fdf3b773c1
James Hurst <james@jameshurst.ca>
2020-03-16 22:56:18 -0400

    Updated libraries

--- scripts/run_ci.sh
+++ scripts/run_ci.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-set -e
+set -ex
 export FASTLANE_SKIP_UPDATE_CHECK=1
 
-if [ -n "$(git ls-files --others --modified --exclude-standard)" ]; then 
+if [ -n "$(git ls-files --others --modified --exclude-standard)" ]; then
   printf "\e[1;31mError: Unclean git environment.\e[0m\n"
   exit -1
 fi
 
 scripts/format.sh
-if [ -n "$(git ls-files --others --modified --exclude-standard)" ]; then 
+if [ -n "$(git ls-files --others --modified --exclude-standard)" ]; then
   printf "\e[1;31mError: Found changes after running 'scripts/format.sh'.\e[0m\n"
   exit -1
 fi
