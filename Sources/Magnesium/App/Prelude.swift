a4e5a298bdda11d98fb792b865b832fe7e0a080d
James Hurst <james@jameshurst.ca>
2020-08-25 20:22:21 -0400

    Update components

--- Sources/Magnesium/App/Prelude.swift
+++ Sources/Magnesium/App/Prelude.swift
@@ -1,7 +1,1 @@
 @_exported import Extensions
-
-// swiftlint:disable:next identifier_name
-func with<Value: AnyObject>(_ value: Value, _ f: (Value) -> Void) -> Value {
-    f(value)
-    return value
-}
--- scripts/run_ci.sh
+++ scripts/run_ci.sh
@@ -10,13 +10,6 @@ fi
 
 scripts/bootstrap.sh
 
-# Xcode 12 has nicer formatter than swiftformat
-# scripts/format.sh
-# if [ -n "$(git ls-files --others --modified --exclude-standard)" ]; then
-#   printf "\e[1;31mError: Found changes after running 'scripts/format.sh'.\e[0m\n"
-#   exit -1
-# fi
-
 tools/mint run swiftlint --strict
 
 xcrun xcodebuild -version
